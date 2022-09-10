e0ef518e3b94e776030cdabda9ae95ac81e74a94
loinguyenqzz@gmail.com <73170897+loinguyenqzz@users.noreply.github.com>
2022-09-10 23:49:35 +0700

    optimize movie card

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import { faPlay, faStar } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './movieCard.module.scss'
 import apiConfig from '../../api/apiConfig'
@@ -20,19 +20,24 @@ export default function MovieCard(props) {
         return subString.slice(0, subString.lastIndexOf(" ")) + '...'
     }
 
-    const briefOverview = truncate(item.overview, 100)
+    const briefTitle = truncate(item.title || item.name, 20)
 
     return (
         <Link to={link}>
             <div className={styles.movieCard} onClick={props.onClick ? () => props.onClick() : null}>
-                <div className={styles.wrapper}>
+                <div className={cx('wrapper-poster')}>
                     <LazyLoadImage effect="blur" src={imgSrc} className={cx('poster')} />
                     <div className={cx('popular')}>Popular : {item.popularity}</div>
-                    <div className={cx('rating')}>Vote: {item.vote_average}</div>
-                    <div className={cx('overview')}>{briefOverview}</div>
                     <FontAwesomeIcon icon={faPlay} className={cx('iconPlay')} />
                 </div>
-                <h4 className={styles.title}>{item.title || item.name}</h4>
+                <h4 className={styles.title}>{briefTitle}</h4>
+                <div className={cx('more-infor')}>
+                    <span className={cx('realease-date')}>{item.release_date || item.first_air_date}</span>
+                    <div className={cx('rating')}>
+                        <FontAwesomeIcon icon={faStar} className={cx('icon')} />
+                        <span className={cx('rate-count')}>{item.vote_average / 2}</span>
+                    </div>
+                </div>
             </div>
         </Link>
     )
